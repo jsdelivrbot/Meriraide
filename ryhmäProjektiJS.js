bdfabe0b2d9f72d49661dbf96049bcff00e7b2c0
tuomaskp123 <45564658+tuomaskp123@users.noreply.github.com>
2018-12-03 17:11:31 +0200

    Update ryhmäProjektiJS.js

--- ryhmäProjektiJS.js
+++ ryhmäProjektiJS.js
@@ -1,26 +1,24 @@
+const junaApi = 'https://rata.digitraffic.fi/api/v1/train-locations/latest/';
 
-fetch('https://rata.digitraffic.fi/api/v1/train-locations/latest/').
+fetch(junaApi).
     then((resp) => resp.json()).
     then(function(api) {
       let trainsArray = api;
 
       for (let i=0;i<trainsArray.length;i++) {
-
-
+        
         let junanNumero = trainsArray[i].trainNumber;
         let junanNopeus = parseInt(trainsArray[i].speed);
-        let koordinaattiX = trainsArray[i].location.coordinates[0];
-        let koordinaattiY = trainsArray[i].location.coordinates[1];
-
-
-        if (junanNopeus > 0) {
+        let koordinaatti1 = trainsArray[i].location.coordinates[0];
+        let koordinaatti2 = trainsArray[i].location.coordinates[1];
+        
+        if (junanNopeus > 0 ) {
           console.log('___________________________');
           console.log('Junan numero: ' + junanNumero);
           console.log('Junan nopeus: ' + junanNopeus);
-          console.log('Koordinaatti x: ' + koordinaattiX);
-          console.log('Koordinaatti y: ' + koordinaattiY);
+          console.log('WGS84: '+ koordinaatti1+' ' +koordinaatti2);
+          //wgs84 pitää muuttaa toiseen formaattiin
         }
-          
       }
 
     });
