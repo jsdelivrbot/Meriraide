96255385d2dc90c78a86472e5283c360039aa36b
tuomaskp123 <45564658+tuomaskp123@users.noreply.github.com>
2018-12-03 17:42:59 +0200

    Update ryhmäProjektiJS.js

--- ryhmäProjektiJS.js
+++ ryhmäProjektiJS.js
@@ -6,20 +6,18 @@ fetch(junaApi).
       let trainsArray = api;
 
       for (let i=0;i<trainsArray.length;i++) {
-        
+
         let junanNumero = trainsArray[i].trainNumber;
         let junanNopeus = parseInt(trainsArray[i].speed);
-        let koordinaatti1 = trainsArray[i].location.coordinates[0];
-        let koordinaatti2 = trainsArray[i].location.coordinates[1];
-        
-        if (junanNopeus > 0 ) { //näyttää liikkuvat junat
+        let junaLat = trainsArray[i].location.coordinates[1];
+        let junaLong = trainsArray[i].location.coordinates[0];
+
+        if (junanNopeus > 0 ) {
           console.log('___________________________');
           console.log('Junan numero: ' + junanNumero);
           console.log('Junan nopeus: ' + junanNopeus);
-          console.log('lat long: '+ koordinaatti1+' ' +koordinaatti2);
-          //latitudet ja longitudet ei välttämättä käy sellaisenaan paikan näyttämiseen kartalla
+          console.log('Junan paikka: '+ junaLat+' ' +junaLong);
         }
       }
 
     });
-
