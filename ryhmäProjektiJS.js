95ee2abf068760accefef6331241926d22e4132f
tuomaskp123 <45564658+tuomaskp123@users.noreply.github.com>
2018-12-03 15:51:42 +0200

    Update ryhmäProjektiJS.js

--- ryhmäProjektiJS.js
+++ ryhmäProjektiJS.js
@@ -7,18 +7,17 @@ fetch('https://rata.digitraffic.fi/api/v1/train-locations/latest/').
 
       for (let i=0;i<trainsArray.length;i++) {
 
-
         let junanNumero = trainsArray[i].trainNumber;
+        let junanNopeus = trainsArray[i].speed;
         let koordinaattiX = trainsArray[i].location.coordinates[0];
         let koordinaattiY = trainsArray[i].location.coordinates[1];
         
-
-
+        console.log('___________________________');
         console.log('Junan numero: '+junanNumero);
+        console.log('Junan nopeus: '+junanNopeus);
         console.log('Koordinaatti x: ' + koordinaattiX);
         console.log('Koordinaatti y: ' + koordinaattiY);
-
-
+          
       }
 
     });
