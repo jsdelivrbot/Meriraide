7530b3d653078ea4de4a421db1cc203eb7f23efe
Cyrbex <45563551+Cyrbex@users.noreply.github.com>
2018-12-04 11:38:16 +0200

    Update Junat.js

--- Junat.js
+++ Junat.js
@@ -1,3 +1,18 @@
+
+let map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([24.41, 62.82]),
+    zoom: 5.8
+  })
+
+});
+
 fetch('https://rata.digitraffic.fi/api/v1/train-locations/latest/').
     then((resp) => resp.json()).
     then(function(api) {
@@ -16,41 +31,26 @@ fetch('https://rata.digitraffic.fi/api/v1/train-locations/latest/').
         console.log('Koordinaatti x: ' + koordinaattiX);
         console.log('Koordinaatti y: ' + koordinaattiY);
 
+        let marker = new ol.Feature({
+          geometry: new ol.geom.Point(
+              ol.proj.fromLonLat([koordinaattiX, koordinaattiY]),
+          ),
+        });
+        let vectorSource = new ol.source.Vector({
+          features: [marker]
+        });
+        let markerVectorLayer = new ol.layer.Vector({
+          source: vectorSource,
+        });
+        map.addLayer(markerVectorLayer);
+
+        marker.setStyle(new ol.style.Style({
+          image: new ol.style.Icon(({
+            crossOrigin: 'anonymous',
+            src: 'junaicon.png'
+          }))
+        }));
       }
 
     });
 
-let map = new ol.Map({
-  target: 'map',
-  layers: [
-    new ol.layer.Tile({
-      source: new ol.source.OSM()
-    })
-  ],
-  view: new ol.View({
-
-    center: ol.proj.fromLonLat([24.41, 62.82]),
-    zoom: 5.8
-  })
-
-});
-
-let marker = new ol.Feature({
-  geometry: new ol.geom.Point(
-      ol.proj.fromLonLat([24.41, 62.82]),
-  ),
-});
-let vectorSource = new ol.source.Vector({
-  features: [marker]
-});
-let markerVectorLayer = new ol.layer.Vector({
-  source: vectorSource,
-});
-map.addLayer(markerVectorLayer);
-
-marker.setStyle(new ol.style.Style({
-  image: new ol.style.Icon(({
-    crossOrigin: 'anonymous',
-    src: 'junaicon.png'
-  }))
-}));
