233e338c2958618520d04eafdaed07704215ee8f
tuomaskp123 <45564658+tuomaskp123@users.noreply.github.com>
2018-12-04 23:48:31 +0200

    Create Juna2.js

---
+++ Juna2.js
@@ -0,0 +1,68 @@
+let map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([24.41, 62.82]),
+    zoom: 5.8
+  })
+
+});
+
+function juna() {
+
+  fetch('https://rata.digitraffic.fi/api/v1/train-locations/latest/').
+      then((resp) => resp.json()).
+      then(function(api) {
+
+        let trainsArray = api;
+
+        for (let i = 0; i < trainsArray.length; i++) {
+
+          let junanNumero = trainsArray[i].trainNumber;
+          let junanNopeus = trainsArray[i].speed;
+          let junaLat = trainsArray[i].location.coordinates[0];
+          let junaLong = trainsArray[i].location.coordinates[1];
+
+          /*
+          console.log('___________________________');
+          console.log('Junan numero: '+ junanNumero);
+          console.log('Junan nopeus: '+ junanNopeus);
+          console.log('Koordinaatti x: ' + junaLat);
+          console.log('Koordinaatti y: ' + junaLong);
+         */
+
+          let marker = new ol.Feature({
+            geometry: new ol.geom.Point(
+                ol.proj.fromLonLat([junaLat, junaLong]),
+            ),
+          });
+          let vectorSource = new ol.source.Vector({
+            features: [marker]
+          });
+          let markerVectorLayer = new ol.layer.Vector({
+            source: vectorSource,
+          });
+
+          map.addLayer(markerVectorLayer);
+
+          setTimeout(function() {
+            map.removeLayer(markerVectorLayer)
+          }, 5000);
+
+
+          marker.setStyle(new ol.style.Style({
+            image: new ol.style.Icon(({
+              crossOrigin: 'anonymous',
+              src: 'junaicon.png'
+            }))
+          }));
+
+        }
+
+      });
+}
+setInterval(juna, 5000);
