a9d4346470856654c1acda1a9547eda25c895a8c
Luhtaje <45563627+Luhtaje@users.noreply.github.com>
2018-12-07 11:21:46 +0200

    Add files via upload

--- ryhmäProjektiJS.js
+++ ryhmäProjektiJS.js
@@ -1,6 +1,6 @@
+const junaApi = 'https://rata.digitraffic.fi/api/v1/train-locations/latest/';
 
-
-fetch('https://rata.digitraffic.fi/api/v1/train-locations/latest/').
+fetch(junaApi).
     then((resp) => resp.json()).
     then(function(api) {
       let trainsArray = api;
@@ -8,16 +8,16 @@ fetch('https://rata.digitraffic.fi/api/v1/train-locations/latest/').
       for (let i=0;i<trainsArray.length;i++) {
 
         let junanNumero = trainsArray[i].trainNumber;
-        let junanNopeus = trainsArray[i].speed;
-        let koordinaattiX = trainsArray[i].location.coordinates[0];
-        let koordinaattiY = trainsArray[i].location.coordinates[1];
-        
-        console.log('___________________________');
-        console.log('Junan numero: '+ junanNumero);
-        console.log('Junan nopeus: '+ junanNopeus);
-        console.log('Koordinaatti x: ' + koordinaattiX);
-        console.log('Koordinaatti y: ' + koordinaattiY);
-          
+        let junanNopeus = parseInt(trainsArray[i].speed);
+        let junaLat = trainsArray[i].location.coordinates[1];
+        let junaLong = trainsArray[i].location.coordinates[0];
+
+        if (junanNopeus > 0 ) {
+          console.log('___________________________');
+          console.log('Junan numero: ' + junanNumero);
+          console.log('Junan nopeus: ' + junanNopeus);
+          console.log('Junan paikka: '+ junaLat+' ' +junaLong);
+        }
       }
 
     });
